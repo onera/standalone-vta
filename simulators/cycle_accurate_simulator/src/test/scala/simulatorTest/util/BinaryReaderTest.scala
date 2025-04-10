90b136e5dc30f778591284cad71bcd3f637e7d1c
lusssse <luce.georgiou@student.isae-supaero.fr>
2025-04-10 17:02:44 +0200

    tests binaryreader

--- simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReaderTest.scala
+++ simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReaderTest.scala
@@ -8,14 +8,23 @@ import BinaryReader._
 class BinaryReaderTest extends AnyFlatSpec with should.Matchers {
 
   //FIXME organize the resource files
-  "BinaryReader" should "decode instruction from binary file" in {
+  "BinaryReader" should "print the decoded instructions in console" in {
     val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin")
     printMapLE(result)
-    //result(0) should equal (Array(0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, -48, 0, 0, 0, 0))
   }
 
-  it should "blabla" in {
-
+  it should "decode correctly the first instruction in a binary file" in {
+    val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin")
+    result(0) should equal (Array(0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, -48, 0, 0, 0, 0))
   }
 
+  it should "decode correctly the last instruction in a binary file" in {
+    val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin")
+    result(result.size - 1) should equal (Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
+  }
+
+  it should "return an error if the file formatting is wrong" in {
+    val result = computeAddresses("examples_compute/compute_smm.json")
+
+  }
 }
