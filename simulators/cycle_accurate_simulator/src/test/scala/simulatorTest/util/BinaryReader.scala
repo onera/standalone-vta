d0f8b238ab56ff8fe68332babd52ea98a56003e4
lusssse <luce.georgiou@student.isae-supaero.fr>
2025-04-14 10:52:25 +0200

    success/failure implementation and offset test

--- simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReader.scala
+++ simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReader.scala
@@ -4,21 +4,7 @@ import java.io.{FileInputStream, IOException}
 import scala.util.{Try, Success, Failure}
 
 object BinaryReader {
-
-  // Open and read the binary file, write the data in an Array[Byte]
   //FIXME essayer d'utiliser Success/Failure
-//  def readBinaryFile(filePath: String): Try[(Array[Byte], Int)] = {
-//    Try {
-//      val inputStreamFile = new FileInputStream(getClass.getClassLoader.getResource(filePath).getFile)
-//      val fileSize = inputStreamFile.available()
-//      val fileContent = new Array[Byte](fileSize)
-//
-//      inputStreamFile.read(fileContent)
-//      inputStreamFile.close()
-//      (fileContent, fileSize) // à modifier en fileContent
-//    }
-//  }
-
   //FIXME add parameters to values
 
   /**
@@ -38,15 +24,15 @@ object BinaryReader {
 
   import DataType._
 
-  //exemple enum
-  def toto(d: DataType.Value): Unit= d match {
-    case INP => ???
-    case WGT => ???
-    case OUT => ???
-    case UOP => ???
-    case INSN => ???
-    case ACC => ???
-  }
+//  //exemple enum
+//  def toto(d: DataType.Value): Unit= d match {
+//    case INP => ???
+//    case WGT => ???
+//    case OUT => ???
+//    case UOP => ???
+//    case INSN => ???
+//    case ACC => ???
+//  }
 
   /**
    * Open and read the binary file, write the data in an Array[Byte]
@@ -62,17 +48,8 @@ object BinaryReader {
       inputStreamFile.read(fileContent)
       inputStreamFile.close()
       fileContent
-
     }
-//    catch {
-//      case e: IOException => {
-//        println(s"Error while reading file : $e")
-//        Array.empty
-//      }
-//    }
   }
-
-
 
   /**
    * Print the Bytes of the input binary file (before Little Endian reversal)
@@ -81,20 +58,18 @@ object BinaryReader {
   def printMapLELE(filePath: String): Try[Unit] = {
     readBinaryFile(filePath) match {
       case Success(fileContent) =>
+        // Print in decimals
         fileContent.foreach(octet => print(s"$octet, "))
+        println("\n")
+        //Print in hexadecimals
+        fileContent.foreach(octet => println(f"$octet%02x "))
         println("\n")
         Success(())
       case Failure(exception) =>
-        println(s"Error while reading file : ${exception.getMessage}")
+        println(s"Error while printing non-reversed file : ${exception.getMessage}")
         Failure(exception)
     }
-    // print en dec
-
-
-//    // print en hex : ok
-//    fileContent.foreach { byte =>
-//      println(f"$byte%02x ")
-    }
+  }
 
 
   /**
@@ -121,85 +96,99 @@ object BinaryReader {
    * @param offset distance between the addresses of different data types
    * @return a Map(Address, Array) that associates the logical address of a vector with its values
    */
-  def computeAddresses(filePath: String, dataType: DataTypeValue, offset: String): Map[BigInt, Array[BigInt]] = { // signature à modifier
-    val groupedBinaryData = if (dataType.doReversal) {
-      reverseLE(readBinaryFile(filePath), dataType)
-    }
-    else {
-      readBinaryFile(filePath).grouped(dataType.bytes).toArray
-    }
-    // Definition of Map
-    val addresses = collection.mutable.Map[BigInt, Array[Byte]]()
-
-    // Filling the Map
-    for (i <- groupedBinaryData.indices) {
-      addresses += ((BigInt(i) + java.lang.Integer.parseInt(offset, 16)) -> groupedBinaryData(i))
-    }
-    // Values type conversion from Byte to BigInt
-    val bigIntMap = addresses.map { case (key, byteValue) =>
-      key -> byteValue.flatMap { byte => Some(BigInt(byte))
-      }.toArray
-    }.toMap
-    bigIntMap
-  }
+//  def computeAddresses(filePath: String, dataType: DataTypeValue, offset: String): Map[BigInt, Array[BigInt]] = { // signature à modifier
+//    val groupedBinaryData = if (dataType.doReversal) {
+//      reverseLE(readBinaryFile(filePath), dataType)
+//    }
+//    else {
+//      readBinaryFile(filePath).grouped(dataType.bytes).toArray
+//    }
+//    // Definition of Map
+//    val addresses = collection.mutable.Map[BigInt, Array[Byte]]()
+//
+//    // Filling the Map
+//    for (i <- groupedBinaryData.indices) {
+//      addresses += ((BigInt(i) + java.lang.Integer.parseInt(offset, 16)) -> groupedBinaryData(i))
+//    }
+//    // Values type conversion from Byte to BigInt
+//    val bigIntMap = addresses.map { case (key, byteValue) =>
+//      key -> byteValue.flatMap { byte => Some(BigInt(byte))
+//      }.toArray
+//    }.toMap
+//    bigIntMap
+//  }
 
   def computeAddressesTry(filePath: String, dataType: DataTypeValue, offset: String): Try[Map[BigInt, Array[BigInt]]] = {
     val groupedBinaryData = if (dataType.doReversal) {
       readBinaryFile(filePath) match {
         case Success(fileContent) =>
-          reverseLE(fileContent, dataType)
-          Success(())
+          // Reverse and group the bytes according to data type
+          Success(reverseLE(fileContent, dataType))
         case Failure(exception) =>
-          println(s"Error while reading file : ${exception.getMessage}")
+          println(s"Error while grouping data (if reversal) : ${exception.getMessage}")
           Failure(exception)
       }
     }
     else {
       readBinaryFile(filePath) match {
         case Success(fileContent) =>
-          fileContent.grouped(dataType.bytes).toArray
-          Success(())
+          // Group the bytes according to data size
+          Success(fileContent.grouped(dataType.bytes).toArray)
         case Failure(exception) =>
-          println(s"Error while reading file : ${exception.getMessage}")
+          println(s"Error while grouping data (no reversal) : ${exception.getMessage}")
           Failure(exception)
       }
     }
-    // Definition of Map
+    // Definition of an empty Map
     val addresses = collection.mutable.Map[BigInt, Array[Byte]]()
 
-    // Filling the Map
-    for (i <- groupedBinaryData.indices) {
-      addresses += ((BigInt(i) + java.lang.Integer.parseInt(offset, 16)) -> groupedBinaryData(i))
+    groupedBinaryData match {
+      case Success(data) =>
+        for (i <- data.indices) {
+          // Filling the Map with the addresses in hexadecimals and its corresponding group of Bytes
+          addresses += ((BigInt(i) + java.lang.Integer.parseInt(offset, 16)) -> data(i))
+        }
+        // Converting the bytes to BigInt
+        val bigIntMap = addresses.map { case (key, byteValue) =>
+            key -> byteValue.flatMap { byte => Some(BigInt(byte))
+            }.toArray
+          }.toMap
+        Success(bigIntMap)
+      case Failure(exception) =>
+        println(s"Error while computing addresses : ${exception.getMessage}")
+        Failure(exception)
     }
-    // Values type conversion from Byte to BigInt
-    val bigIntMap = addresses.map { case (key, byteValue) =>
-      key -> byteValue.flatMap { byte => Some(BigInt(byte))
-      }.toArray
-    }.toMap
-    bigIntMap
   }
 
   /**
    * Print the Map with the instructions encoded in a format CHISEL can read
    * @param map a Map that associates the logical addresses of a vector and its values
    */
-  def printMapLE(map: Map[BigInt, Array[BigInt]], dataType: DataTypeValue): Unit = {
+  def printMapLE(map: Try[Map[BigInt, Array[BigInt]]], dataType: DataTypeValue): Try[Unit] = {
     println("Content of the Map :")
-    // print en dec
-    if (dataType.id == 5 || dataType.id == 3) {
-      map.foreach { case (key, values) =>
-        println(s"Instruction index : $key")
-        println(s"Values : ${values.mkString(", ")}")
-        println("---")
-      }
-    }
-    else {
-       // print en hex
-          map.foreach { case (key, values) =>
+
+    map match {
+      case Success(data) =>
+        // Print in decimals for Instructions and UOPs
+        if (dataType.id == 5 || dataType.id == 3) {
+          data.foreach { case (key, values) =>
+            println(s"Instruction index : $key")
+            println(s"Values : ${values.mkString(", ")}")
+            println("---")
+          }
+        }
+        // Print in hexadecimals for matrix data
+        else {
+          data.foreach { case (key, values) =>
             val hexKey = Integer.toHexString(key.toInt)
             println(s"Logical address (Hex) : ${"0" * (8 - hexKey.length)}$hexKey")
             println(s"Values (Hex) : ${values.map(_.toInt & 0xFF).map("%02x".format(_)).mkString(", ")}")
           }
+        }
+        Success(())
+      case Failure(exception) =>
+        println(s"Error while printing reversed file : ${exception.getMessage}")
+        Failure(exception)
     }
   }
 }
--- simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReaderTest.scala
+++ simulators/cycle_accurate_simulator/src/test/scala/simulatorTest/util/BinaryReaderTest.scala
@@ -2,14 +2,15 @@ package simulatorTest.util
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-
 import BinaryReader._
+
+import scala.util.{Success, Failure}
 
 class BinaryReaderTest extends AnyFlatSpec with should.Matchers {
 
   //FIXME organize the resource files
   "BinaryReader" should "print the decoded instructions in console" in {
-    val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
+    val result = computeAddressesTry("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
     printMapLE(result, DataType.INSN)
   }
 
@@ -18,16 +19,22 @@ class BinaryReaderTest extends AnyFlatSpec with should.Matchers {
   }
 
   it should "print the INP data encoded in a format computable by CHISEL" in {
-    printMapLE(computeAddresses("examples_compute/input_lenet5_layer1.bin", DataType.INP, "00000000"), DataType.INP)
+    printMapLE(computeAddressesTry("examples_compute/input_lenet5_layer1.bin", DataType.INP, "00000000"), DataType.INP)
   }
 
   it should "decode correctly the first vector of INP (16 Bytes) in a binary file" in {
-    val result =  computeAddresses("examples_compute/input_lenet5_layer1.bin", DataType.INP, "00000000")
-    result(0) should equal (Array(2, -1, -4, 1, 2, 1, 1, 1, -4, -1, 1, 2, -3, -4, 1, 1))
+    val result =  computeAddressesTry("examples_compute/input_lenet5_layer1.bin", DataType.INP, "00000000")
+    result match {
+      case Success(data) =>
+        Success(data(0) should equal (Array(2, -1, -4, 1, 2, 1, 1, 1, -4, -1, 1, 2, -3, -4, 1, 1)))
+      case Failure(exception) =>
+        println(s"Error while computing addresses for INP : ${exception.getMessage}")
+        Failure(exception)
+    }
   }
 
   it should "decode correctly the first vector of WGT (256 Bytes) in a binary file" in {
-    val result =  computeAddresses("examples_compute/weight_lenet5_layer1.bin", DataType.WGT, "00000000")
+    val result =  computeAddressesTry("examples_compute/weight_lenet5_layer1.bin", DataType.WGT, "00000000")
     val inputWGTHex = Array(
       "01", "02", "FF", "FE", "FC", "01", "01", "01", "FE", "00", "FE", "FE", "FC", "01", "00",
       "00", "FC", "01", "FE", "02", "FC", "02", "FC", "FE", "00", "FD", "00", "FE", "FF", "FF",
@@ -219,21 +226,61 @@ class BinaryReaderTest extends AnyFlatSpec with should.Matchers {
       val decimal = java.lang.Integer.parseInt(hex, 16)
       if (decimal >= 128) BigInt(decimal - 256) else BigInt(decimal)
     }
-    result(0) should equal (wgtFirstBlock)
+    result match {
+      case Success(data) =>
+        Success(data(0) should equal (wgtFirstBlock))
+      case Failure(exception) =>
+        println(s"Error while computing addresses for WGT : ${exception.getMessage}")
+        Failure(exception)
+    }
   }
 
   it should "decode correctly the first instruction (16 Bytes) in a binary file" in {
-    val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
-    result(0) should equal (Array(0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, -48, 0, 0, 0, 0))
+    val result = computeAddressesTry("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
+    result match {
+      case Success(data) =>
+        Success(data(0) should equal (Array(0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, -48, 0, 0, 0, 0)))
+      case Failure(exception) =>
+        println(s"Error while computing addresses for instructions : ${exception.getMessage}")
+        Failure(exception)
+    }
   }
 
   it should "decode correctly the last instruction in a binary file" in {
-    val result = computeAddresses("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
-    result(result.size - 1) should equal (Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3))
+    val result = computeAddressesTry("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
+    result match {
+      case Success(data) =>
+        Success(data(data.size - 1) should equal (Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3)))
+      case Failure(exception) =>
+        println(s"Error while computing addresses for instructions : ${exception.getMessage}")
+        Failure(exception)
+    }
   }
 
+  it should "return the same value if an offset is or isn't used" in {
+    val resultOffset = computeAddressesTry("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00001000")
+    val resultWithoutOffset = computeAddressesTry("examples_compute/instructions_lenet5_layer1.bin", DataType.INSN, "00000000")
+    resultOffset match {
+      case Success(dataOffset) =>
+        resultWithoutOffset match {
+          case Success(dataWithoutOffset) =>
+            val idx = java.lang.Integer.parseInt("00001000", 16)
+            Success(dataOffset(idx) should equal (dataWithoutOffset(0)))
+          case Failure(exception) =>
+            println(s"Error while computing addresses for INP without an offset : ${exception.getMessage}")
+            Failure(exception)
+        }
+      case Failure(exception) =>
+        println(s"Error while computing addresses for INP with an offset : ${exception.getMessage}")
+        Failure(exception)
+    }
+  }
+
+
+
   it should "return an error if the file formatting is wrong" in {
-    val result = computeAddresses("examples_compute/compute_smm.json", DataType.INSN, "00000000")
+    val result = computeAddressesTry("examples_compute/compute_smm.json", DataType.INSN, "00000000")
+
 
   }
 }
