793cdd51c8eec15bddc942aba3144040fab45a5d
FAURE-GIGNOUX Anthony <anthony.faure-gignoux@onera.fr>
2025-05-13 15:31:48 +0200

    Update average_pooling.py

--- compiler/data_definition/average_pooling.py
+++ compiler/data_definition/average_pooling.py
@@ -55,6 +55,27 @@ def reference_average_pooling(matrix, kernel_size=2, stride=2, debug=False):
     return pooled_matrix, channel_size, pooled_height
 
 
+def avg_pool_sram(matrix, kernel_size=2, stride=2, debug=False):
+    # Get the height (H) and width (W) of the input matrix
+    H, W = matrix.shape
+
+    # Compute the size of the square channel tensor (sqrt(H))
+    channel_size = int(np.sqrt(H))  # Each column will be reshaped into channel_size x channel_size
+    
+    # Calculate the dimensions of the pooled matrix after the pooling operation
+    pooled_height = (channel_size - kernel_size) // stride + 1
+
+    # Add 2 by 2
+    for idx in range(0, H, 2):
+        matrix[idx] = matrix[idx, :] + matrix[idx+1, :]
+
+    # Add line + shift right
+    for i in range(0, channel_size - kernel_size + 1, stride): # Row
+        for k in range(0, channel_size - kernel_size + 1, stride): # Col
+            matrix[i*channel_size + k] = np.floor( (matrix[i*channel_size + k, :] + matrix[(i+1)*channel_size + k, :])/(kernel_size**2) )
+
+    return matrix, channel_size, pooled_height
+
 # Index computation
 # -------------
 def average_pooling_indexes(in_tensor_size=4, out_tensor_size=2, kernel_size=2, stride=2):
@@ -94,14 +115,21 @@ def average_pooling_indexes(in_tensor_size=4, out_tensor_size=2, kernel_size=2, 
 # -------------
 if __name__ == '__main__':
     kernel = 2
-    stride = 1
+    stride = 2
 
-    test_matrix = np.random.randint(-128, 127, size=(3**2, 2), dtype=np.int8)
+    # INIT MATRIX
+    test_matrix = np.random.randint(-128, 127, size=(16, 2), dtype=np.int32)
     print(f"The matrix: \n{test_matrix} \n")
 
+    # REFERENCE
     pooled_matrix, in_tensor, out_tensor = reference_average_pooling(test_matrix, kernel, stride, debug=True)
     print(f"The pooled matrix: \n{pooled_matrix} \n")
 
+    # SRAM RESULT
+    sram_matrix, _, _ = avg_pool_sram(test_matrix, kernel, stride, debug=True)
+    print(f"The SRAM matrix: \n{sram_matrix} \n")
+
+    # INDEXES COMPUTATION
     indexes = average_pooling_indexes(in_tensor, out_tensor, kernel, stride)
     print("The indexes:")
     for index in indexes:
