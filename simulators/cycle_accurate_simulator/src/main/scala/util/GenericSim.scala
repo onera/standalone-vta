48eaf63ced3caabc76e4d339282d2b1570c47807
lusssse <luce.georgiou@student.isae-supaero.fr>
2025-05-22 11:21:09 +0200

    modification genericsim (plus une classe abstraite) pour test computesim

--- simulators/cycle_accurate_simulator/src/main/scala/util/GenericSim.scala
+++ simulators/cycle_accurate_simulator/src/main/scala/util/GenericSim.scala
@@ -7,30 +7,30 @@ import org.scalatest.flatspec.AnyFlatSpec
 import vta.DefaultPynqConfig
 import vta.util.config._
 
-abstract class GenericSim[T <: Module, P <: PeekPokeTester[T], C <: Parameters] extends AnyFlatSpec with ChiselScalatestTester {
-  implicit val p: Parameters = new DefaultPynqConfig
-  val defaultOpts = Seq(TreadleBackendAnnotation)
-}
-
-class GenericMain[T <: Module, P <: PeekPokeTester[T], C <: Parameters](tag : String, dutFactory : (Parameters) => T,
-                                                                       testerFactory : (T) => P)
-  extends GenericSim[T, P, C] {
-
-  behavior of tag
-  it should "not have expect violations" in {
-    test(dutFactory(p)).withAnnotations(defaultOpts).runPeekPoke(testerFactory)
-  }
-}
-
-//class GenericSim[T <: Module, P <: PeekPokeTester[T], C <: Parameters](tag : String, dutFactory : (Parameters) => T,
-//                                                                       testerFactory : (T) => P, isLongTest : Boolean = false)
-//  extends AnyFlatSpec with ChiselScalatestTester {
-//
+//abstract class GenericSim[T <: Module, P <: PeekPokeTester[T], C <: Parameters] extends AnyFlatSpec with ChiselScalatestTester {
 //  implicit val p: Parameters = new DefaultPynqConfig
 //  val defaultOpts = Seq(TreadleBackendAnnotation)
+//}
+//
+//class GenericMain[T <: Module, P <: PeekPokeTester[T], C <: Parameters](tag : String, dutFactory : (Parameters) => T,
+//                                                                       testerFactory : (T) => P)
+//  extends GenericSim[T, P, C] {
 //
 //  behavior of tag
 //  it should "not have expect violations" in {
 //    test(dutFactory(p)).withAnnotations(defaultOpts).runPeekPoke(testerFactory)
 //  }
 //}
+
+class GenericSim[T <: Module, P <: PeekPokeTester[T], C <: Parameters](tag : String, dutFactory : (Parameters) => T,
+                                                                       testerFactory : (T) => P, isLongTest : Boolean = false)
+  extends AnyFlatSpec with ChiselScalatestTester {
+
+  implicit val p: Parameters = new DefaultPynqConfig
+  val defaultOpts = Seq(TreadleBackendAnnotation)
+
+  behavior of tag
+  it should "not have expect violations" in {
+    test(dutFactory(p)).withAnnotations(defaultOpts).runPeekPoke(testerFactory)
+  }
+}
